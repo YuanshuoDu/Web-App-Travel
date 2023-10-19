c4fe2a3698a722978ad5e3d7f9fc0aca3940a924
Stevendu666 <923352830@qq.com>
2023-10-19 13:17:32 +0100

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
 
-const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+
+import Login from "./componets/login";
+import Signup from "./componets/signup";
+import Nav from "./componets/nav"
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Routes>
+        <Route path="/account/login" element={<Login/>}/>
+        <Route path="/account/signup" element={<Signup/>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "./global.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { reducers } from "./redux/reducers"
+import thunk from "redux-thunk"
+import {BrowserRouter} from "react-router-dom"
+import { createStore, applyMiddleware, compose} from "redux"
+import {GoogleOAuthProvider} from "@react-oauth/google"
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk)))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+      <GoogleOAuthProvider
+                clientId={`ENTER_YOUR_CLIENTID`}>
+       <App />
+       </GoogleOAuthProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
