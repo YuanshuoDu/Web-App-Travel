f299c0fd9d1f706a9cd5cf285fbcab2874425b1b
Stevendu666 <923352830@qq.com>
2023-10-26 22:29:31 +0100

    mongo update

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use("/users", userRoutes)
 
 const PORT = process.env.PORT || 5000;
-const MONGOOSE_URL = "mongodb://localhost:27017/LSYSTEM"
+const MONGOOSE_URL = 'mongodb+srv://test:yVF6YxZPFBdIMswk@travelappcluster.1xu2xdn.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
 
 mongoose.connect(MONGOOSE_URL, {useNewUrlParser: true})
 .then(()=> app.listen(PORT, ()=>{
