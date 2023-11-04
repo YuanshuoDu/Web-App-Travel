f997c79bc435bf54cc23315202c03e98371e8d8d
deivit21 <david29.st@gmail.com>
2023-11-04 13:54:58 +0000

    IconButton options

--- client/src/components/story_detail/story_detail.js
+++ client/src/components/story_detail/story_detail.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Paper, Typography, CircularProgress, Divider, Chip, Card, CardActions, IconButton } from '@material-ui/core/';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
@@ -7,6 +7,9 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import { getStory } from '../../redux/actions/stories';
 import useStyles from './styles';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const StoryDetail = () => {
   const { story } = useSelector((state) => state.stories);
@@ -15,17 +18,29 @@ const StoryDetail = () => {
   const { id } = useParams();
   var userLikePost = false;
 
-    var handleButtonClick = () => {
-        if (userLikePost) {
-            userLikePost = false;
-        } else {
-            userLikePost = true;
-        }
-    };
+
+
+  var handleButtonClick = () => {
+    if (userLikePost) {
+      userLikePost = false;
+    } else {
+      userLikePost = true;
+    }
+  };
 
   useEffect(() => {
     dispatch(getStory(id));
   }, [id]);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
 
   //if (!story) return null;
@@ -50,8 +65,24 @@ const StoryDetail = () => {
   return (
     <Card className={classes.storyPaper} elevation={6}>
       <div className={classes.card}>
+        <IconButton
+          className={classes.optionsButton}
+          aria-controls="card-options-menu"
+          aria-haspopup="true"
+          onClick={handleMenuClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="card-options-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => alert('Update story')}>Edit story</MenuItem>
+          <MenuItem onClick={() => alert('Delete story')}>Delete story</MenuItem>
+        </Menu>
         <div className={classes.textSection}>
-
           <Typography variant="h4" component="h2">{story.title}</Typography>
           <div className={classes.creationInfo}>
             <Typography variant="body1">
@@ -62,27 +93,27 @@ const StoryDetail = () => {
           <TagList tags={story.tags} />
           <Typography gutterBottom variant="body1" component="p">{story.message}</Typography>
           <CardActions className={classes.likeButton}>
-                    {userLikePost ?
-                        (
-                            <IconButton size="small" onClick={handleButtonClick}>
-                                <FavoriteIcon color="error" />
-                            </IconButton>
-                        ) :
-                        (
-                            <IconButton size="small" onClick={handleButtonClick}>
-                                <FavoriteBorderOutlinedIcon />
-                            </IconButton>
-                        )
-                    }
-                    <Typography variant="body2">{story.likeCount}</Typography>
-                </CardActions>
+            {userLikePost ?
+              (
+                <IconButton size="small" onClick={handleButtonClick}>
+                  <FavoriteIcon color="error" />
+                </IconButton>
+              ) :
+              (
+                <IconButton size="small" onClick={handleButtonClick}>
+                  <FavoriteBorderOutlinedIcon />
+                </IconButton>
+              )
+            }
+            <Typography variant="body2">{story.likeCount}</Typography>
+          </CardActions>
           <Divider style={{ margin: '20px' }} />
         </div>
         <div className={classes.imageSection}>
           <img className={classes.picture} src={story.selectedPicture || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={story.title} />
         </div>
 
-       
+
       </div>
     </Card>
   );
