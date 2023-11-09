b488732fa7802df4928b2fa7a91ffaef78790022
Stevendu666 <923352830@qq.com>
2023-11-09 11:40:30 +0000

    copy from dev

--- client/src/components/story_detail/story_detail.js
+++ client/src/components/story_detail/story_detail.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Paper, Typography, CircularProgress, Divider, Chip, Card, CardActions, IconButton, Container, Box } from '@material-ui/core/';
-import React, { useEffect, useState } from 'react';
-import { Paper, Typography, CircularProgress, Divider, Chip, Card, CardActions, IconButton, Container, Box } from '@material-ui/core/';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import moment from 'moment';
 import { useParams, useHistory, Link } from 'react-router-dom';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
-import { getStory, deleteStory } from '../../redux/actions/stories';
 import { getStory, deleteStory } from '../../redux/actions/stories';
 import useStyles from './styles';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -20,21 +16,12 @@ import Nav from "../nav"
 
 const StoryDetail = () => {
   const { selectedStory, stories, isLoading } = useSelector((state) => state.stories);
-  const { selectedStory, stories, isLoading } = useSelector((state) => state.stories);
   const dispatch = useDispatch();
   const classes = useStyles();
-  const navigate = useNavigate();
   const navigate = useNavigate();
   const { id } = useParams();
   var userLikePost = false;
 
-  var handleButtonClick = () => {
-    if (userLikePost) {
-      userLikePost = false;
-    } else {
-      userLikePost = true;
-    }
-  };
   var handleButtonClick = () => {
     if (userLikePost) {
       userLikePost = false;
@@ -111,64 +98,7 @@ const StoryDetail = () => {
     );
   }
 
-  if (isLoading || !selectedStory) {
-    return (
-      <div style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <Container maxWidth="xl">
-          <Container maxWidth="lg">
-            <Nav />
-            <div style={{ marginTop: '60px' }} />
-            <Paper elevation={6} className={classes.loadingPaper}>
-              <CircularProgress size="3em" />
-            </Paper>
-            <Divider style={{ margin: '40px' }} />
-          </Container>
-        </Container>
-      </div>
-    );
-  }
-
   return (
-    <div style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <Container maxWidth="xl">
-        <Container maxWidth="lg">
-          <Nav />
-          <main>
-            <div style={{ marginTop: '60px' }} />
-
-            <Card className={classes.storyPaper} elevation={6}>
-              <div className={classes.card}>
-
-                <div className={classes.textSection}>
-                  <div className={classes.titleRow}>
-                    <Typography variant="h4" component="h2">{selectedStory.title || ""}</Typography>
-                    <IconButton
-                      className={classes.optionsButton}
-                      aria-controls="card-options"
-                      aria-haspopup="true"
-                      onClick={handleMenuClick}
-                    >
-                      <MoreVertIcon />
-                    </IconButton>
-                    <Menu
-                      id="card-options"
-                      anchorEl={anchorEl}
-                      open={Boolean(anchorEl)}
-                      onClose={handleClose}
-                    >
-                      <MenuItem onClick={() => handleEdit(selectedStory._id)}>Edit story</MenuItem>
-                      <MenuItem onClick={() => handleDelete(selectedStory._id)}>Delete story</MenuItem>
-                    </Menu>
-                  </div>
-                  <div className={classes.creationInfo}>
-                    <Typography variant="body1">
-                      Created by: {selectedStory.creator || ""}
-                    </Typography>
-                    <Typography variant="body2">{moment(selectedStory.createdAt || Date()).fromNow()}</Typography>
-                  </div>
-                  <TagList tags={selectedStory.tags || []} />
-                  <Typography gutterBottom variant="body1" component="p">{selectedStory.message || ""}</Typography>
-                  <CardActions className={classes.likeButton}>
     <div style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Container maxWidth="xl">
         <Container maxWidth="lg">
@@ -220,31 +150,7 @@ const StoryDetail = () => {
                           <FavoriteBorderOutlinedIcon />
                         </IconButton>
                       )
-                      (
-                        <IconButton size="small" onClick={handleButtonClick}>
-                          <FavoriteIcon color="error" />
-                        </IconButton>
-                      ) :
-                      (
-                        <IconButton size="small" onClick={handleButtonClick}>
-                          <FavoriteBorderOutlinedIcon />
-                        </IconButton>
-                      )
                     }
-                    <Typography variant="body2">{selectedStory.likeCount || 0}</Typography>
-                  </CardActions>
-                  <Divider style={{ margin: '20px' }} />
-                </div>
-                <div className={classes.imageSection}>
-                  <img className={classes.picture} src={selectedStory.selectedPicture || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={selectedStory.title || ""} />
-                </div>
-              </div>
-            </Card>
-            <Divider style={{ margin: '40px' }} />
-          </main>
-        </Container>
-      </Container>
-    </div>
                     <Typography variant="body2">{selectedStory.likeCount || 0}</Typography>
                   </CardActions>
                   <Divider style={{ margin: '20px' }} />
--- client/src/redux/reducers/auth.js
+++ client/src/redux/reducers/auth.js
@@ -1,23 +1,10 @@
-import * as actionType from "../const/actionsTypes";
+import * as actionType from '../const/actionsTypes';
 
 const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case actionType.AUTH:
-      localStorage.setItem("user_info", JSON.stringify({ ...action?.data }));
-      // Check if firstname and lastname exist in action.data
-      const { firstname, lastname, ...restData } = action.data;
-
-      // Store only firstname and lastname in localStorage
-      localStorage.setItem(
-        "user_info",
-        JSON.stringify({ firstname, lastname })
-      );
-
-      // Optionally, log firstname and lastname
-      console.log("firstname:", firstname);
-      console.log("lastname:", lastname);
-
-      console.log(action.data);
+      localStorage.setItem('user_info', JSON.stringify({ ...action?.data }));
+      console.log(action.data)
       return { ...state, authData: action.data };
     case actionType.LOGOUT:
       localStorage.clear();
